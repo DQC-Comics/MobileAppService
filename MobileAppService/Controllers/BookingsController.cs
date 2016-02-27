7f4cebf2bc4d9734139bc113ce5654652aa8b157
Jonas Karlman <jonas@kwiboo.se>
2016-02-27 11:34:32 +0100

    Import Azure Mobile App project

---
+++ MobileAppService/Controllers/BookingsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MobileAppService.Controllers
+{
+    using System.Threading.Tasks;
+    using MobileAppService.Models;
+    using DqccomicsMobileapiService;
+    using DqccomicsMobileapiService.Models;
+    using Microsoft.Azure.Mobile.Server.Config;
+
+    [MobileAppController]
+    public class BookingsController : ApiController
+    {
+        [Route("api/Bookings")]
+        public async Task<IEnumerable<Booking>> PostBookings(BookingUser user)
+        {
+            var client = new Unknowntype();
+            var bookings = await client.Bookings.GetBookingsWithOperationResponseAsync();
+            return bookings.Body.Where(_ => _.CustomerId == user.UserId).Select(_ => new Booking
+            {
+                HeroName = _.Heroes.FirstOrDefault()?.Name ?? string.Empty,
+                Description = _.Description,
+                StartTime = _.StartTime,
+                EndTime = _.EndTime
+            });
+        }
+
+        [Route("api/Bookings/Create")]
+        public async Task<Booking> PostCreate(BookingNew booking)
+        {
+            var client = new Unknowntype();
+
+            var heroes = await client.Heroes.PostHeroesWithOperationResponseAsync(new ApiHeroSearch { StartTime = booking.StartTime, EndTime = booking.EndTime, Text = booking.Description });
+            
+            var hero = heroes.Body.FirstOrDefault();
+
+            var apiBooking = new ApiBooking
+                                 {
+                                    Id = 0,
+                                     CustomerId = booking.UserId,
+                                     StartTime = booking.StartTime,
+                                     EndTime = booking.EndTime,
+                                     Description = booking.Description,
+                                     Heroes = new List<ApiHeroBasic>() { new ApiHeroBasic() { Id = hero.Id, Name = hero.Name } },
+                                     Location = string.Empty,
+                                     City = string.Empty,
+                                     Country = string.Empty,
+                                     Status = string.Empty
+                                 };
+
+            try
+            {
+                var created = await client.Bookings.PostBookingWithOperationResponseAsync(apiBooking);
+            }
+            catch (Exception ex)
+            {}
+
+            return new Booking();
+        }
+    }
+}
---
+++ MobileAppService/Models/Booking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileAppService.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public string HeroName { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTimeOffset? StartTime { get; set; }
+
+        public DateTimeOffset? EndTime { get; set; }
+    }
+
+    public class BookingUser
+    {
+        public string UserId { get; set; }
+    }
+
+    public class BookingNew : Booking
+    {
+        public string UserId { get; set; }
+    }
+}
